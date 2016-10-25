70e1e48c6f506143be31c73ab2493dc37e1547a1
Ruhulphp <raruhulcse@gmail.com>
2016-10-25 11:30:21 +0600

    add function.php file

---
+++ function.php
@@ -0,0 +1,16 @@
+<?php
+
+$globalName = "Zoe";
+
+function sayHello() {
+$localName = "Harry";
+echo "Hello, $localName!<br>";
+}
+
+sayHello();
+echo "The value of \$globalName is: '$globalName'<br>";
+echo "The value of \$localName is: '$localName'<br>";
+
+?>
+
+
